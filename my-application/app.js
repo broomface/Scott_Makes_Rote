c3555c8b85d994aed346743d33cffb4e7655e24e
nrfi@sbcglobal.net <scott.m.rushford@gmail.com>
2017-11-25 14:22:45 -0700

    build server in 730

--- my-application/app.js
+++ my-application/app.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 app.disable('x-powered-by')
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.get('/ping', (req, res, next) => {
-  res.json({ message: 'Pong!'})
+  res.json({message: 'Pong!'})
 })
 
 app.use((err, req, res, next) => {
   const status = err.status || 500
-  res.status(status).json({error : err})
+  res.status(status).json({error: err})
 })
 
 app.use((req, res, next) => {
-  res.status(404).json({error: {message : 'Not found.'}})
+  res.status(404).json({error: {message: 'Not found.'}})
 })
 
 
